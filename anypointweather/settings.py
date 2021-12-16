6c3794d959e91f7b520c24efbdc8bcfc19a08291
Patel-Pramod <pramodtirole9575@gmail.com>
2021-12-16 18:00:49 +0530

    commit

--- anypointweather/settings.py
+++ anypointweather/settings.py
@@ -125,5 +125,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT =os.path.join(BASE_DIR ,'staticfiles')
