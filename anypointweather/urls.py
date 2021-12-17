68cc19950d74ddcfe69ac17f44781f453f9651ef
Patel-Pramod <pramodtirole9575@gmail.com>
2021-12-17 16:32:25 +0530

    commit

--- anypointweather/urls.py
+++ anypointweather/urls.py
@@ -26,6 +26,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.Home),
     path('weather/', views.weather),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
