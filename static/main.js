ab2317b1fe0da8f2d7f693322a3c4914cd848adc
Patel-Pramod <pramodtirole9575@gmail.com>
2021-12-24 15:26:54 +0530

    commit

--- static/main.js
+++ static/main.js
@@ -1,3 +1,21 @@
+const month = new Array();
+month[0] = "Jan";
+month[1] = "Feb";
+month[2] = "Mar";
+month[3] = "Apr";
+month[4] = "May";
+month[5] = "Jun";
+month[6] = "Jul";
+month[7] = "Aug";
+month[8] = "Sep";
+month[9] = "Oct";
+month[10] = "Nov";
+month[11] = "Dec";
+
+let Months = month[d.getMonth()];
+
+C_Date.innerHTML = dates + Months;
+
 const cityName = document.getElementById('cityName');
 const submitBtn = document.getElementById('submitBtn');
 
